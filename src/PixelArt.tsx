4ef86b5587234ad5b3b9b523b0a880d22a4af52b
Sitakkun <t5k1m7.swim@gmail.com>
2022-01-17 20:50:39 +0900

    input color test

--- src/PixelArt.tsx
+++ src/PixelArt.tsx
@@ -5,7 +5,30 @@ type Props = {
     pixel: number
 }
 
-const csscolors = require('css-color-names');
+//cssカラーリストの読み取り
+let csscolors = require('css-color-names');
+
+//キーを含んだ配列に変換
+let array = Object.keys(csscolors).map((k) => (
+    ({ key: k, value: csscolors[k]})
+));
+
+// hexコード順に並び替える。(明→暗の順番)
+array.sort(function(a,b) {
+    const aHex:number = parseInt(a.value.substring(1,3),16) + parseInt(a.value.substring(3,5),16) + parseInt(a.value.substring(5),16);
+    const bHex:number = parseInt(b.value.substring(1,3),16) + parseInt(b.value.substring(3,5),16) + parseInt(b.value.substring(5),16);
+    if(aHex > bHex) {
+        return -1;
+    } else {
+        return 1
+    }
+});
+
+//連想配列に再変換
+csscolors = Object.assign({}, ...array.map((item) => ({
+    [item.key]: item.value,
+})));
+
 
 const PixelArt: React.VFC<Props> = ({ pixel }) => {
     const[currentColor,setCurrentColor] = useState("red");
@@ -36,6 +59,15 @@ const PixelArt: React.VFC<Props> = ({ pixel }) => {
         setCurrentColor(event.currentTarget.id);
     }
 
+    // input color から値を取得
+    const changeColorFromInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrentColor(event.target.value);
+        const pickColorList = document.getElementById("color-list");
+        const colorData = document.createElement("option");
+        colorData.setAttribute("value",event.target.value);
+        pickColorList?.appendChild(colorData);
+    }
+
     //PixelArtテーブルの<td>にIDを付与するために利用。
     const newID = () => {
         const id:string = "row" + lastID;
@@ -57,6 +89,7 @@ const PixelArt: React.VFC<Props> = ({ pixel }) => {
                 cnt = 0;
             }
         }
+        trs.push(<tr>{ tds }</tr>)
         return trs;
     }
 
@@ -90,6 +123,13 @@ const PixelArt: React.VFC<Props> = ({ pixel }) => {
                     </tr>
                 </tbody>
             </table>
+            <input type="color" name="colorPicker" list="color-list" onBlur={ changeColorFromInput }/>
+            <datalist id="color-list">
+                <option value="#ff0000"></option>
+                <option value="#00ff00"></option>
+                <option value="#0000ff"></option>
+                <option value="red"></option>
+            </datalist>
         </div>
         </>
     )
